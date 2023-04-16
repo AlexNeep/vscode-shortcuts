8644ac4ad2edf4224646a83e6792dc98da488b91
Alex Neep <neepalex@gmail.com>
2023-04-16 15:03:39 +0100

    Copy vercel docs

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,49 +1,18 @@
-import type { EntryContext } from "@vercel/remix";
+import { handleRequest } from "@vercel/remix";
 import { RemixServer } from "@remix-run/react";
-import { renderToPipeableStream } from "react-dom/server";
-import { PassThrough } from "stream";
+import type { EntryContext } from "@vercel/remix";
 
-// import { handleRequest } from '@vercel/remix';
-
-const ABORT_DELAY = 20000;
-
-export default function handleRequest(
+export default function (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const callbackName = "onShellReady";
-
-  return new Promise((resolve, reject) => {
-    let didError = false;
-
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        [callbackName]() {
-          let body = new PassThrough();
-
-          responseHeaders.set("Content-Type", "text/html");
-
-          resolve(
-            // @ts-expect-error
-            new Response(body, {
-              status: didError ? 500 : responseStatusCode,
-              headers: responseHeaders,
-            })
-          );
-          pipe(body);
-        },
-        onShellError(err: unknown) {
-          reject(err);
-        },
-        onError(error: unknown) {
-          didError = true;
-          console.error(error);
-        },
-      }
-    );
-    setTimeout(abort, ABORT_DELAY);
-  });
+  const remixServer = <RemixServer context={remixContext} url={request.url} />;
+  return handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixServer
+  );
 }
